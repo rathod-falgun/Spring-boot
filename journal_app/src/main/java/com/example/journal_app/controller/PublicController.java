5a7963f6434ac8abaf5e61e84d901a2f2b4248ee
Falgun Rathod <rathodfalgun05@gmail.com>
2026-06-02 11:32:53 +0530

    Journal Appplication

--- journal_app/src/main/java/com/example/journal_app/controller/PublicController.java
+++ journal_app/src/main/java/com/example/journal_app/controller/PublicController.java
@@ -1,5 +1,6 @@
 package com.example.journal_app.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
